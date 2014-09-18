01e86616a13dd5adeff5decb5e837d17eb11aeb7
enbake-2 <sprasad@enbake.com>
2014-09-18 14:06:49 +0530

    Edit functionality

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,37 +1,34 @@
 angular.module('DemoAppRate.controllers', [])
-  .controller('TaskController', function($scope, $route, $routeParams){
-    $scope.tasks = {};
+  .controller('TaskController',['$scope',function($scope, $route, $routeParams){
+    $scope.tasks = [];
     $scope.empty = false;
     $scope.name = '';
     $scope.description = '';
-    $scope.tasks = [
-          {name:'Crete a form',description:'Allow a user to create a form.'},
-          {name:'List form',description:'List all the forms Created.'},
-          {name:'Edit form',description:'Edit a form data.'}
-        ];
+    $scope.id = null;
+    $scope.form_action = "Create";
 
-   $scope.create = function create(){
-      $scope.tasks.push($scope.task); 
-      alert("Created successfully!");
+    $scope.create = function create(){
+        if($scope.task.id !== undefined){
+          $scope.tasks[$scope.task.id] = $scope.task
+          $scope.task = {};
+          alert("Updated successfully");
+        }else{
+          $scope.tasks.push($scope.task); 
+          $scope.task = {};
+          alert("Created successfully");
+        }
+        $scope.form_action = "Create";
     };
 
-    $scope.editTask = function(index){
-      alert($scope.tasks[index].name);
-      $scope.tasks.name = $scope.tasks[index].name;
-      $scope.tasks.description = $scope.tasks[index].description;
-      //alert(JSON.stringify($scope.task));
-      //$scope.tasks.push($scope.task); 
-      //alert("Created successfully!");
+    $scope.editTask = function (index){
+      $scope.task.name = $scope.tasks[index].name;
+      $scope.task.description = $scope.tasks[index].description;
+      $scope.task.id = index;
+      $scope.form_action = "Update";
     };
-  
-    // $scope.addItem = function(item){
-    //   $scope.items.push(item);
-    //   $scope.newItem = null;
-    // };
-
     $scope.removeTask = function(index){
       $scope.tasks.splice(index, 1);
     };
 
-  });
+  }]);
   
